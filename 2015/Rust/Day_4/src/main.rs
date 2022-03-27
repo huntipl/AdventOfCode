8aafc99801a28551c43c8ee4f46819f378aeb714
Peter <git@hunti.net>
2022-03-27 17:42:06 +0100

    working single block algo

--- 2015/Rust/Day_4/src/main.rs
+++ 2015/Rust/Day_4/src/main.rs
@@ -4,6 +4,9 @@
 
 // Debug info: https://rosettacode.org/wiki/MD5/Implementation_Debug
 
+#![allow(unused_macros)]
+#![allow(non_snake_case)]
+
 #[cfg(target_endian = "big")]
 fn print_endian() {
     println!("Big endian")
@@ -68,7 +71,7 @@ macro_rules! print32h {
     ($b_arr:expr) => {
         let mut i = 0;
         for b in &$b_arr {
-            print!("M{i}:\t{b:08x}");
+            print!("{i}:\t{b:08x}");
             i += 1;
             print!("\n");
         }
@@ -94,58 +97,59 @@ fn main() {
 
     // let _desired_pre = "00000";
 
-    print_endian();
-    //
-    // MD5
-    // peter => "51dc30ddc473d43a6011e9ebba6ca770"
-
-    // let test = "peter".to_string();
-    let test = "".to_string();
-    // let test = "They are deterministic".to_string();
-
-    let input = test;
+    let input = String::from("");
     // let _desired_result = "51dc30ddc473d43a6011e9ebba6ca770";
 
-    // result - 128bit, 4x32bit
-
-    println!(":::Input::: {input}");
+    println!(":::Input::: '{input}'");
+    
     let mut input_bytes: Vec<u8> = Vec::new();
     input_bytes = input.into_bytes();
-    println!(":::Converting to bytes::: {input_bytes:?}");
+    println!(":::Input bytes:::");
+    printx!(input_bytes);
 
     let original_byte_len: u64 = input_bytes.len() as u64;
     let to_append: u8 = 56 - original_byte_len as u8;
 
-    input_bytes.append(&mut vec![1 << 7]);
-    input_bytes.append(&mut vec![0; to_append as usize - 1]);
+    let mut append_bytes: Vec<u8> = vec![];
 
-    input_bytes.append(&mut (original_byte_len * 8).to_be_bytes().to_vec());
+    append_bytes.append(&mut vec![1 << 7]);
+    append_bytes.append(&mut vec![0; to_append as usize - 1]);
+    append_bytes.append(&mut (original_byte_len * 8).to_le_bytes().to_vec());
 
-    let new_len = input_bytes.len();
-
-    // println!(":::Appending bytes::: {input_bytes:?} len:{new_len}");
     println!(":::Appending bytes:::");
-    println!(":::Control print of bits:::");
+    printx!(append_bytes);
+    input_bytes.append(&mut append_bytes);
 
+    println!(":::Control print of bits:::");
     // printb!(input_bytes);
-    // printx!(input_bytes);
+    printx!(input_bytes);
+
+    
+
+    // ------------------Input Preparation complete------------------
 
     // M
     let M = vec![&input_bytes];
     let mut MM: Vec<u32> = vec![];
     for i in (0..input_bytes.len()).step_by(4) {
-        let new_number: u32 = ((input_bytes[i] as u32) << 24)
-            + ((input_bytes[i + 1] as u32) << 16)
-            + ((input_bytes[i + 2] as u32) << 8)
-            + ((input_bytes[i + 3] as u32) << 0);
+        let new_number: u32 = 
+              ((input_bytes[i + 0] as u32) << 0)
+            + ((input_bytes[i + 1] as u32) << 8)
+            + ((input_bytes[i + 2] as u32) << 16)
+            + ((input_bytes[i + 3] as u32) << 24);
         MM.push(new_number);
     }
 
-    // println!("32bit array:");
-    // print32h!(MM);
+    // dec -> bits -> byte -> 32bit
+    // 10 -> 00001010 -> 0a -> 00 00 00 0a (Big Endian)
+    // 0a 00 00 00 (little endian)
 
-    // Preparation complete;
+    // in out "" exapmple:
+    // 80 00 00 00 ... -> 00 00 00 80 
 
+
+    println!(":::Chunk split in words: [M0..M15]:::");
+    print32h!(MM);
 
     // those are reversed from the RFC doc as they are written in low order byte first
     let mut A = u32::from_str_radix("67452301", 16).unwrap();
@@ -153,18 +157,17 @@ fn main() {
     let mut C = u32::from_str_radix("98BADCFE", 16).unwrap();
     let mut D = u32::from_str_radix("10325476", 16).unwrap();
 
+
+    // println!("::: A B C D PRE:::");
+    // println!("A: {A:#08x}\nB: {B:#08x}\nC: {C:#08x}\nD: {D:#08x}");
+
+
+    // T matrix, sine function
     let T: Vec<u32> = (0..64)
         .map(|x| (((x + 1) as f64).sin().abs() * 2_u64.pow(32) as f64) as u32)
         .collect();
-        
-    
 
-    // panic!();
-    
-    // println!("A: {A:#08x}\nB: {B:#08x}\nC: {C:#08x}\nD: {D:#08x}");
-    // println!("A: {A:#?}\nB: {B:#?}\nC: {C:#?}\nD: {D:#?}");
-
-    // println!("T matrix:");
+    // println!(":::T matrix:::");
     // print32h!(T);
     // print32!(T);
 
@@ -178,7 +181,18 @@ fn main() {
         }
     }).collect();
     let s: Vec<u32> = vec![7,12,17,22,7,12,17,22,7,12,17,22,7,12,17,22,5,9,14,20,5,9,14,20,5,9,14,20,5,9,14,20,4,11,16,23,4,11,16,23,4,11,16,23,4,11,16,23,6,10,15,21,6,10,15,21,6,10,15,21,6,10,15,21];
-    let i: Vec<u32> = vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64];
+    // let i: Vec<u32> = vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64];
+    let i: Vec<u32> = (1..=64).collect();
+
+    // println!(":::k vector:::");
+    // println!("{k:?}");
+
+    // println!(":::s vector:::");
+    // println!("{s:?}");
+
+    // println!(":::i vector:::");
+    // println!("{i:?}");
+
 
     fn F(X: u32, Y: u32, Z: u32) -> u32 {
         X & Y | !X & Z
@@ -272,7 +286,6 @@ fn main() {
     // Process in Blocks M[0..N] - we only have one 512bit block for our case
     // let X = &M[0];
 
-    println!("k: {k:?}");
 
     let AA = A;
     let BB = B;
@@ -282,63 +295,117 @@ fn main() {
     // Round 1
 
     for r in 0..4 {
-        // println!("Round 1, : {:?}", (r*4,r*4+1,r*4+2,r*4+3));
-        A = R1(&A, &B, &C, &D, k[r*4], s[r*4], i[r*4], &MM, &T);
-        D = R1(&D, &A, &B, &C, k[r*4+1], s[r*4+1], i[r*4+1], &MM, &T);
-        C = R1(&C, &D, &A, &B, k[r*4+2], s[r*4+2], i[r*4+2], &MM, &T);
-        B = R1(&B, &C, &D, &A, k[r*4+3], s[r*4+3], i[r*4+3], &MM, &T);
+        // println!(":::Round 1:{r}:::");
+        let mut step = 0;
+
+        A = R1(&A, &B, &C, &D, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        D = R1(&D, &A, &B, &C, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        C = R1(&C, &D, &A, &B, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        B = R1(&B, &C, &D, &A, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        
     }
+    // println!("After round 1: {A:x?} {B:x?} {C:x?} {D:x?}");
 
     // Round 2
 
-    for r in 5..8 {
-        // println!("Round 2, : {:?}", (r*4,r*4+1,r*4+2,r*4+3));
-        A = R2(&A, &B, &C, &D, k[r*4], s[r*4], i[r*4], &MM, &T);
-        D = R2(&D, &A, &B, &C, k[r*4+1], s[r*4+1], i[r*4+1], &MM, &T);
-        C = R2(&C, &D, &A, &B, k[r*4+2], s[r*4+2], i[r*4+2], &MM, &T);
-        B = R2(&B, &C, &D, &A, k[r*4+3], s[r*4+3], i[r*4+3], &MM, &T);
+    for r in 4..8 {
+        // println!(":::Round 2:{r}:::");
+        let mut step = 0;
+
+        A = R2(&A, &B, &C, &D, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        D = R2(&D, &A, &B, &C, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        C = R2(&C, &D, &A, &B, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        B = R2(&B, &C, &D, &A, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
     }
+    // println!("After round 2: {A:x?} {B:x?} {C:x?} {D:x?}");
 
     // Round 3
 
-    for r in 9..12 {
-        // println!("Round 3, : {:?}", (r*4,r*4+1,r*4+2,r*4+3));
-        A = R3(&A, &B, &C, &D, k[r*4], s[r*4], i[r*4], &MM, &T);
-        D = R3(&D, &A, &B, &C, k[r*4+1], s[r*4+1], i[r*4+1], &MM, &T);
-        C = R3(&C, &D, &A, &B, k[r*4+2], s[r*4+2], i[r*4+2], &MM, &T);
-        B = R3(&B, &C, &D, &A, k[r*4+3], s[r*4+3], i[r*4+3], &MM, &T);
+    for r in 8..12 {
+        // println!(":::Round 3:{r}:::");
+        let mut step = 0;
+
+        A = R3(&A, &B, &C, &D, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        D = R3(&D, &A, &B, &C, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        C = R3(&C, &D, &A, &B, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        B = R3(&B, &C, &D, &A, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
     }
+    // println!("After round 3: {A:x?} {B:x?} {C:x?} {D:x?}");
 
     // Round 4
 
-    for r in 13..16 {
-        // println!("Round 4, : {:?}", (r*4,r*4+1,r*4+2,r*4+3));
-        A = R4(&A, &B, &C, &D, k[r*4], s[r*4], i[r*4], &MM, &T);
-        D = R4(&D, &A, &B, &C, k[r*4+1], s[r*4+1], i[r*4+1], &MM, &T);
-        C = R4(&C, &D, &A, &B, k[r*4+2], s[r*4+2], i[r*4+2], &MM, &T);
-        B = R4(&B, &C, &D, &A, k[r*4+3], s[r*4+3], i[r*4+3], &MM, &T);
+    for r in 12..16 {
+        // println!(":::Round 4:{r}:::");
+        let mut step = 0;
+
+        A = R4(&A, &B, &C, &D, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        D = R4(&D, &A, &B, &C, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        C = R4(&C, &D, &A, &B, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
+        step += 1;
+        B = R4(&B, &C, &D, &A, k[r*4+step], s[r*4+step], i[r*4+step], &MM, &T);
+        // println!("k={} s={} i={} T={:x?}", k[r*4+step], s[r*4+step], i[r*4+step], T[i[r*4+step] as usize -1]);
+        // println!("After {step} step: {A:x?} {B:x?} {C:x?} {D:x?}");
     }
+    // println!("After round 4: {A:x?} {B:x?} {C:x?} {D:x?}");
+
+    // println!("Final prints:");
+    // println!("After all rounds: A {A:x?} {B:x?} {C:x?} {D:x?}");
+    // println!("After all rounds: AA {AA:x?} {BB:x?} {CC:x?} {DD:x?}");
 
     let A = A.wrapping_add(AA);
     let B = B.wrapping_add(BB);
     let C = C.wrapping_add(CC);
     let D = D.wrapping_add(DD);
 
-    println!("{A:08x} {B:08x} {C:08x} {D:08x}");
+    // println!("After final increment {A:x?} {B:x?} {C:x?} {D:x?}");
 
+    let A = A.to_be();
+    let B = B.to_be();
+    let C = C.to_be();
+    let D = D.to_be();
 
+    println!(":::MD5:::");
+    println!("{A:08x}{B:08x}{C:08x}{D:08x}");
 
-    // time to mess about with the data
-    // for x in (0..input_bytes.len()).step_by(2){
-    //     println!("{},{}: {} {}", x, x+1, input_bytes[x], input_bytes[x+1]);
-
-    //         /* Copy block i into X. */
-    //      For j = 0 to 15 do
-    //      Set X[j] to M[i*16+j].
-    //    end /* of loop on j */
-    // }
-
-    // let X = M
-
-    // use something smarter like: https://stackoverflow.com/questions/57029974/how-to-split-string-into-chunks-in-rust-to-insert-spaces
 }
